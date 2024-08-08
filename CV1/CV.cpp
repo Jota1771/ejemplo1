8a154174e5b76696da3d159f2995431f662b62b9
Jorge Barrantes Lopez <jorge.barrantes.lopez@est.una.ac.cr>
2024-08-08 16:44:05 -0600

    Agregar archivos de proyecto.

---
+++ CV1/CV.cpp
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------LIBRERIAS------------------------------------------------------------------------------*/
+
+#include <iostream>
+
+#include <opencv2/core.hpp>      
+#include <opencv2/imgcodecs.hpp>	
+#include <opencv2/highgui.hpp>   
+#include <opencv2/imgproc.hpp>	
+
+
+// Definicion de espacios
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+	
+
+
+	return 0;
+}
